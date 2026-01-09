2b15ec568473c3fb204a11381f91b87c6c38c0d5
Vegard Pareli Seines <vegsei@gmail.com>
2026-01-09 12:55:23 +0100

    working on audio fix

--- hosts/work/P14S/configuration.nix
+++ hosts/work/P14S/configuration.nix
@@ -52,21 +52,21 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+# Aktiver lyd med PipeWire
+security.rtkit.enable = true; # Gir PipeWire sanntidsprioritet
+services.pipewire = {
+  enable = true;
+  alsa.enable = true;
+  alsa.support32Bit = true;
+  pulse.enable = true;
+  # Her kan du legge til lav-forsinkelse innstillinger om nødvendig:
+  extraConfig.pipewire."92-low-latency" = {
+    "context.properties" = {
+      "default.clock.rate" = 48000;
+      "default.clock.quantum" = 1024; # Øk denne verdien (f.eks 2048) hvis det fortsatt hakker
+    };
   };
+};
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
