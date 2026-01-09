3cae77f7648dab7e4bbe3faa5ec4481776178a44
Vegard Pareli Seines <vegsei@gmail.com>
2026-01-09 22:28:46 +0100

    wip add legion config

--- flake.nix
+++ flake.nix
@@ -29,5 +29,23 @@
 	}
       ];
     };
+    nixosConfigurations.legioni5 = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./hosts/personal/legioni5/configuration.nix
+	./modules/defaults.nix
+	./modules/desktop/gnome.nix
+	./modules/desktop/defaults.nix
+
+        home-manager.nixosModules.home-manager
+	{
+	  home-manager.useGlobalPkgs = true;
+	  home-manager.useUserPackages = true;
+	  home-manager.extraSpecialArgs = { inherit inputs; };
+	  home-manager.users.vegard = import ./modules/home.nix;
+	}
+      ];
+    };
   };
 }
