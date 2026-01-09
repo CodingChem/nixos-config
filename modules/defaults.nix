502fe553473ba340cda800e101910027c95e8127
Vegard Pareli Seines <vegsei@gmail.com>
2026-01-09 14:33:17 +0100

    fix user configuration

--- modules/defaults.nix
+++ modules/defaults.nix
@@ -1,17 +1,28 @@
 { config, pkgs, ... }:
 
 {
+  # System-settings
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  
   # Networking
   networking.networkmanager.enable = true;
+  
   # Set your time zone.
   time.timeZone = "Europe/Oslo";
+  
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
+  
   # Configure console keymap
   console.keyMap = "no";
+  
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
+
+  # Define the user
+  users.users.vegard = {
+    isNormalUser = true;
+    description = "Vegard Seines";
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
 }
-
-
--- modules/home.nix
+++ modules/home.nix
@@ -1,32 +1,31 @@
-
 { config, pkgs, ... }:
 
 {
-  home.stateVersion = "25.11";
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.vegard = {
-    isNormalUser = true;
-    description = "Vegard Seines";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-    #  thunderbird
-    ];
-  };
+  # User setup
+  home.username = "vegard";
+  home.homeDirectory = "/home/vegard";
+  home.stateVersion = "25.11"; 
+
+  # Apps
+  home.packages = with pkgs; [
+  ];
+
+  # program settings
   programs = {
+    home-manager.enable = true;
+
     neovim = {
       enable = true;
       defaultEditor = true;
     };
+
     git = {
       enable = true;
-      settings = {
-        user = {
-	  name = "Vegard Pareli Seines";
-	  email = "vegsei@gmail.com";
-	};
-	init.defaultBranch = "main";
+      userName = "Vegard Pareli Seines";
+      userEmail = "vegsei@gmail.com";
+      extraConfig = {
+        init.defaultBranch = "main";
       };
     };
-    home-manager.enable = true;
   };
 }
