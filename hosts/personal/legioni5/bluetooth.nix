0101136da6ea91aefe7912ca679e3c3e959984f6
Vegard Pareli Seines <vegsei@gmail.com>
2026-01-10 12:32:42 +0100

    fix syntax

--- hosts/personal/legioni5/bluetooth.nix
+++ hosts/personal/legioni5/bluetooth.nix
@@ -1,26 +1,24 @@
-
 { config, pkgs, ... }:
 
 {
-
-  # I stedet for et komplisert skript, forteller vi driveren direkte om ID-en
-  # Dette tilsvarer det du gjorde manuelt, men skjer idet modulen laster.
+  # 1. Tving ID-en inn i btusb-driveren med en gang modulen laster
+  # Dette simulerer det manuelle skriptet ditt, men skjer automatisk.
   boot.extraModprobeConfig = ''
     install btusb ${pkgs.kmod}/bin/modprobe --ignore-install btusb; echo 0489 e111 > /sys/bus/usb/drivers/btusb/new_id
   '';
 
+  # 2. Last modulen og legg til parametere som hindrer krasj
   boot.kernelModules = [ "btusb" ];
-  
-  # Hindrer at laptopen prøver å styre strømmen til Bluetooth (som ofte dreper MediaTek)
   boot.blacklistedKernelModules = [ "ideapad_laptop" ];
-  
   boot.kernelParams = [ 
     "usbcore.autosuspend=-1" 
     "btusb.enable_autosuspend=n"
-    "btusb.disable_scofix=1"
-    "i915.force_probe=7f2f" 
+    "btusb.disable_scofix=1" 
   ];
 
-  # Aktiver standard bluetooth-støtte
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
+  # 3. Aktiver Bluetooth-tjenesten
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+} # <--- Sjekk at du har denne lukkeklammen!
