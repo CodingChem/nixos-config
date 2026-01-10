fe2b0227b1df03afbfc58049bbdddc7e86536734
Vegard Pareli Seines <vegsei@gmail.com>
2026-01-10 12:58:41 +0100

    try fix

--- hosts/personal/legioni5/bluetooth.nix
+++ hosts/personal/legioni5/bluetooth.nix
@@ -1,4 +1,12 @@
-{ config, pkgs, ... }:
+ { config, pkgs, ... }:
 
 {
+  # Configure kernel parameters for Bluetooth
+  boot.extraModprobeConfig = ''
+    options btusb enable_autosuspend=n
+  '';
+
+  # Ensure the correct firmware and drivers are available (usually standard, but good to be safe)
+  hardware.enableAllFirmware = true;
+  
 }
