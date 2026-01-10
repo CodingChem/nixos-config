e51a38a90d7607c164da56bdf2725b7eaaa25512
Vegard Pareli Seines <vegsei@gmail.com>
2026-01-10 13:29:12 +0100

    fix niri

--- modules/desktop/niri.nix
+++ modules/desktop/niri.nix
@@ -2,7 +2,7 @@
 
 {
   # 1. Ensure the apps you bind are actually installed
-  home.packages = with pkgs; [
+  home-manager.packages = with pkgs; [
     ghostty
     xwayland-satellite
     # alacritty # You can remove this if you fully switched to Ghostty
