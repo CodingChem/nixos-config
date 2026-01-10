90bdfdc4addf11423a7f905746733c063a544d19
Vegard Pareli Seines <vegsei@gmail.com>
2026-01-10 13:11:19 +0100

    try fix

--- hosts/personal/legioni5/bluetooth.nix
+++ hosts/personal/legioni5/bluetooth.nix
@@ -1,47 +1,19 @@
  { config, pkgs, ... }:
 
 {
-  # ... existing configuration ...
+# 1. Fix the "False Identity" bug where GNOME thinks the Bluetooth chip is a Camera/MP3 player
+  services.udev.extraHwdb = ''
+    # Match Foxconn / Hon Hai Wireless_Device (0489:e111)
+    usb:v0489pE111*
+     ID_GPHOTO2=0
+     ID_MTP_DEVICE=0
+     ID_MEDIA_PLAYER=0
+  '';
 
-  systemd.services.fix-bluetooth-mt7925 = {
-    description = "Fix MediaTek Bluetooth on Legion Pro 5i by resetting btusb";
-    
-    # Run this after the basic system is ready
-    after = [ "multi-user.target" ];
-    wantedBy = [ "multi-user.target" ];
-    
-    serviceConfig = {
-      Type = "oneshot";
-      # We need root to mess with kernel modules
-      User = "root";
-      
-      # The script sequence:
-      # 1. Stop bluetooth service to release the device
-      # 2. Unload the btusb driver
-      # 3. Wait 5 seconds (critical for the hardware to reset)
-      # 4. Reload btusb (with autosuspend disabled, just in case)
-      # 5. Start bluetooth service again
-      ExecStart = pkgs.writeShellScript "reset-btusb" ''
-        echo "Stopping Bluetooth service..."
-        ${pkgs.systemd}/bin/systemctl stop bluetooth.service
-        
-        echo "Unloading btusb module..."
-        ${pkgs.kmod}/bin/modprobe -r btusb
-        
-        echo "Waiting for hardware to reset..."
-        sleep 5
-        
-        echo "Reloading btusb module..."
-        ${pkgs.kmod}/bin/modprobe btusb enable_autosuspend=n
-        
-        echo "Restarting Bluetooth service..."
-        ${pkgs.systemd}/bin/systemctl start bluetooth.service
-      '';
-      
-      # Don't consider the service "failed" if it exits successfully
-      RemainAfterExit = true;
-    };
-  };
+  # 2. Keep the "Clean" power management fix just in case (optional but recommended)
+  boot.extraModprobeConfig = ''
+    options btusb enable_autosuspend=n
+  '';
 
   # Ensure you have the necessary firmware packages
   hardware.enableAllFirmware = true;
