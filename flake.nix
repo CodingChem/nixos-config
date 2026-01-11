465ce5a3bbc93af266902ea4c3ed3d5c6cec6523
Vegard Pareli Seines <vegsei@gmail.com>
2026-01-11 01:57:32 +0100

    gemini trying

--- flake.nix
+++ flake.nix
@@ -8,13 +8,9 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    dms = {
-      url = "github:AvengeMedia/DankMaterialShell/stable";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { self, nixpkgs, home-manager, dms, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations.P14S = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
@@ -42,7 +38,6 @@
         ./modules/defaults.nix
         ./modules/game.nix
         
-	inputs.dms.nixosModules.default
         ./modules/desktop/dms.nix
         
         ./modules/desktop/gnome.nix
