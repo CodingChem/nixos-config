e204bfd47a4a1da8e985de870204316ce042c054
Vegard Pareli Seines <vegsei@gmail.com>
2026-01-11 23:16:51 +0100

    gnome

--- flake.nix
+++ flake.nix
@@ -35,26 +35,12 @@
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [
-        # # 2. Add the COSMIC module provided by the flake
-        # nixos-cosmic.nixosModules.default
-        #
-        # # 3. CRITICAL: Add Binary Cache to avoid compiling from source
-        # {
-        #   nix.settings = {
-        #     substituters = [ "https://cosmic.cachix.org/" ];
-        #     trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjXPe96e9mVBdPx154KZQ7D0UVFb4=" ];
-        #   };
-        # }
-
         ./hosts/personal/legioni5/configuration.nix
         ./modules/defaults.nix
         ./modules/game.nix
-        
-        # ./modules/desktop/dms.nix
-        
-        # 4. Swapped gnome.nix for cosmic.nix here
         ./modules/desktop/cosmic.nix 
         ./modules/desktop/defaults.nix
+	./modules/desktop/gnome.nix
         
         home-manager.nixosModules.home-manager
         {
