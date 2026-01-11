7275f46f096e21c0ab0ff9584df434812a56008b
Vegard Pareli Seines <vegsei@gmail.com>
2026-01-11 11:25:54 +0100

    trying cosmic

--- flake.nix
+++ flake.nix
@@ -9,14 +9,14 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # 1. Add the COSMIC flake input
-    nixos-cosmic = {
-      url = "github:lilyinstarlight/nixos-cosmic";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # # 1. Add the COSMIC flake input
+    # nixos-cosmic = {
+    #   url = "github:lilyinstarlight/nixos-cosmic";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
   };
 
-  outputs = { self, nixpkgs, home-manager, nixos-cosmic, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations.P14S = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
@@ -40,22 +40,22 @@
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [
-        # 2. Add the COSMIC module provided by the flake
-        nixos-cosmic.nixosModules.default
-
-        # 3. CRITICAL: Add Binary Cache to avoid compiling from source
-        {
-          nix.settings = {
-            substituters = [ "https://cosmic.cachix.org/" ];
-            trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjXPe96e9mVBdPx154KZQ7D0UVFb4=" ];
-          };
-        }
+        # # 2. Add the COSMIC module provided by the flake
+        # nixos-cosmic.nixosModules.default
+        #
+        # # 3. CRITICAL: Add Binary Cache to avoid compiling from source
+        # {
+        #   nix.settings = {
+        #     substituters = [ "https://cosmic.cachix.org/" ];
+        #     trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjXPe96e9mVBdPx154KZQ7D0UVFb4=" ];
+        #   };
+        # }
 
         ./hosts/personal/legioni5/configuration.nix
         ./modules/defaults.nix
         ./modules/game.nix
         
-        ./modules/desktop/dms.nix
+        # ./modules/desktop/dms.nix
         
         # 4. Swapped gnome.nix for cosmic.nix here
         ./modules/desktop/cosmic.nix 
--- modules/desktop/cosmic.nix
+++ modules/desktop/cosmic.nix
@@ -1,22 +1,12 @@
 { config, pkgs, lib, ... }:
 
 {
-  # --- System settings ---
-  
-  # Enable the COSMIC Desktop Environment.
-  # Note: This requires the nixos-cosmic overlay/module to be present in your flake.
   services.desktopManager.cosmic.enable = true;
   services.displayManager.cosmic-greeter.enable = true;
-
-  # Configure keymap (applies to TTY and cosmic-greeter)
   services.xserver.xkb = {
     layout = "no";
     variant = "";
   };
-
-  # --- Nvidia 5070Ti Optimization ---
-  # COSMIC is Wayland-native. While it handles Nvidia well out of the box,
-  # ensuring hardware acceleration and proprietary drivers are active is key.
   services.xserver.videoDrivers = [ "nvidia" ]; 
   
   hardware.graphics = {
@@ -37,23 +27,6 @@
     # It uses TOML files in ~/.config/cosmic.
     # However, we still want to configure GTK for non-COSMIC apps (Firefox, etc.)
     # so they look consistent with the desktop.
-    
-    gtk = {
-      enable = true;
-      theme = {
-        name = "Adwaita-dark";
-        package = pkgs.gnome-themes-extra;
-      };
-      iconTheme = {
-        name = "Papirus-Dark"; # A popular, flat icon theme that fits COSMIC well
-        package = pkgs.papirus-icon-theme;
-      };
-      cursorTheme = {
-        name = "Bibata-Modern-Ice";
-        package = pkgs.bibata-cursors;
-        size = 24;
-      };
-    };
 
     # Install useful COSMIC utilities if they aren't pulled in by the desktop manager
     home.packages = with pkgs; [
