6580652c431ae91d4987b3e2d9c726c77e6d8328
Vegard Pareli Seines <vegsei@gmail.com>
2026-01-11 01:07:10 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -8,53 +8,62 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # Input is correct
     niri.url = "github:YaLTeR/niri";
   };
 
-  outputs = { self, nixpkgs, home-manager, niri, ... }@inputs: {
+  # We use @inputs to access niri safely
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations.P14S = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [
         ./hosts/work/P14S/configuration.nix
-	./modules/defaults.nix
-	./modules/desktop/gnome.nix
-	./modules/desktop/defaults.nix
+        ./modules/defaults.nix
+        ./modules/desktop/gnome.nix
+        ./modules/desktop/defaults.nix
 
         home-manager.nixosModules.home-manager
-	{
-	  home-manager.useGlobalPkgs = true;
-	  home-manager.useUserPackages = true;
-	  home-manager.extraSpecialArgs = { inherit inputs; };
-	  home-manager.users.vegard = import ./modules/home.nix;
-	}
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = { inherit inputs; };
+          home-manager.users.vegard = import ./modules/home.nix;
+        }
       ];
     };
+
     nixosConfigurations.legioni5 = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [
         ./hosts/personal/legioni5/configuration.nix
-	./modules/defaults.nix
-	./modules/game.nix
-	./modules/desktop/niri.nix
-	./modules/desktop/gnome.nix
-	./modules/desktop/defaults.nix
+        ./modules/defaults.nix
+        ./modules/game.nix
+        
+        # Your Niri configuration file
+        ./modules/desktop/niri.nix 
+        
+        ./modules/desktop/gnome.nix
+        ./modules/desktop/defaults.nix
 
-	niri.nixosModules.niri
+        # FIX 1: Use inputs.niri explicitly to avoid errors
+        inputs.niri.nixosModules.niri
 
         home-manager.nixosModules.home-manager
-	{
-	  home-manager.useGlobalPkgs = true;
-	  home-manager.useUserPackages = true;
-	  home-manager.extraSpecialArgs = { inherit inputs; };
-	  home-manager.users.vegard = {
-	    imports = [
-	      ./modules/home.nix
-	      niri.homeModules.niri
-	    ];
-	  };
-	}
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = { inherit inputs; };
+          home-manager.users.vegard = {
+            imports = [
+              ./modules/home.nix
+              # FIX 2: Use inputs.niri here too
+              inputs.niri.homeModules.niri
+            ];
+          };
+        }
       ];
     };
   };
