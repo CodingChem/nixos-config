34725be98ece2c5613f35ace1a4a1dd2c8171331
Vegard Pareli Seines <vegsei@gmail.com>
2026-01-11 01:15:02 +0100

    update nix

--- flake.nix
+++ flake.nix
@@ -3,17 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    niri.url = "github:YaLTeR/niri";
   };
 
-  # 1. ADD 'niri' HERE vvv
-  outputs = { self, nixpkgs, home-manager, niri, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations.P14S = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
@@ -35,16 +32,15 @@
 
     nixosConfigurations.legioni5 = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      
-      # 2. PASS 'niri' HERE vvv
-      # This ensures modules receive the EVALUATED flake, not just the source
-      specialArgs = { inherit inputs niri; };
-      
+      specialArgs = { inherit inputs; };
       modules = [
         ./hosts/personal/legioni5/configuration.nix
         ./modules/defaults.nix
         ./modules/game.nix
+        
+        # Load your local Niri config
         ./modules/desktop/niri.nix
+        
         ./modules/desktop/gnome.nix
         ./modules/desktop/defaults.nix
 
@@ -52,10 +48,7 @@
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-          
-          # 3. PASS 'niri' TO HOME MANAGER TOO vvv
-          home-manager.extraSpecialArgs = { inherit inputs niri; };
-          
+          home-manager.extraSpecialArgs = { inherit inputs; };
           home-manager.users.vegard = {
             imports = [
               ./modules/home.nix
--- modules/desktop/niri.nix
+++ modules/desktop/niri.nix
@@ -1,24 +1,21 @@
-# 1. ADD 'niri' TO ARGUMENTS vvv
-{ config, pkgs, niri, ... }: 
+{ config, pkgs, ... }:
 
 {
-  # 2. USE 'niri' DIRECTLY (It is now the evaluated flake)
-  imports = [ niri.nixosModules.niri ];
-
+  # 1. Enable Niri System Session (Login Screen)
   programs.niri.enable = true;
 
+  # 2. Configure User Settings
   home-manager.users.vegard = { config, pkgs, ... }: {
     
-    # 3. USE 'niri' HERE TOO
-    imports = [ niri.homeModules.niri ];
-
+    # Packages to install
     home.packages = with pkgs; [
       xwayland-satellite
+      waybar
     ];
 
     programs.niri = {
       enable = true;
-      package = pkgs.niri;
+      package = pkgs.niri; # Use standard Nixpkgs version
 
       settings = {
         input = {
@@ -46,26 +43,34 @@
           };
         };
 
-        binds = with config.lib.niri.actions; {
-          "Mod+Shift+E".action = quit;
-          "Mod+Q".action = close-window;
-          "Mod+Return".action = spawn "ghostty"; 
-          "Mod+D".action = spawn "fuzzel"; 
+        # --- KEYBINDINGS (Standard Format) ---
+        binds = {
+          "Mod+Shift+E".action.quit = {};
+          "Mod+Q".action.close-window = {};
           
-          "Mod+H".action = focus-column-left;
-          "Mod+L".action = focus-column-right;
-          "Mod+J".action = focus-window-down;
-          "Mod+K".action = focus-window-up;
-          "Mod+Ctrl+H".action = move-column-left;
-          "Mod+Ctrl+L".action = move-column-right;
-          "Mod+Home".action = focus-column-first;
-          "Mod+End".action = focus-column-last;
+          # Spawning apps
+          "Mod+Return".action.spawn = "ghostty";
+          "Mod+D".action.spawn = "fuzzel";
+
+          # Navigation
+          "Mod+H".action.focus-column-left = {};
+          "Mod+L".action.focus-column-right = {};
+          "Mod+J".action.focus-window-down = {};
+          "Mod+K".action.focus-window-up = {};
           
-          "Print".action = screenshot;
-          "Ctrl+Print".action = screenshot-screen;
-          "Alt+Print".action = screenshot-window;
+          "Mod+Ctrl+H".action.move-column-left = {};
+          "Mod+Ctrl+L".action.move-column-right = {};
+          
+          "Mod+Home".action.focus-column-first = {};
+          "Mod+End".action.focus-column-last = {};
+
+          # Screenshots
+          "Print".action.screenshot = {};
+          "Ctrl+Print".action.screenshot-screen = {};
+          "Alt+Print".action.screenshot-window = {};
         };
 
+        # --- Startup ---
         spawn-at-startup = [
           { command = [ "xwayland-satellite" ]; }
           { command = [ "waybar" ]; }
