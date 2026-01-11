f85888b3c587a1f93ff4ff09aeb27f7462146266
Vegard Pareli Seines <vegsei@gmail.com>
2026-01-11 22:49:04 +0100

    networking patches

--- modules/defaults.nix
+++ modules/defaults.nix
@@ -6,7 +6,13 @@
   
   # Networking
   networking.networkmanager.enable = true;
-  
+  # Enable BBR Congestion Control
+  boot.kernel.sysctl = {
+    "net.core.default_qdisc" = "fq";
+    "net.ipv4.tcp_congestion_control" = "bbr";
+  };
+  networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
+  networking.networkmanager.wifi.backend = "iwd";
   # Set your time zone.
   time.timeZone = "Europe/Oslo";
   
