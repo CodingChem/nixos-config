e10040d05198bd9c52b26604ac171e34f6da9475
Vegard Pareli Seines <vegsei@gmail.com>
2026-01-12 12:34:40 +0100

    fix jetbrains on cosmic

--- modules/programming/kotlin.nix
+++ modules/programming/kotlin.nix
@@ -1,9 +1,31 @@
 { config, pkgs, ... }:
 
+let
+  # 1. Definer flaggene du fant på GitHub
+  waylandJavaFlags = "-Dawt.toolkit.name=WLToolkit -Dsun.java2d.vulkan=true -Dsun.java2d.vulkan.accelsd=true";
+
+  # 2. En hjelpefunksjon som "wrapper" programmet med flaggene
+  # Den tar inn en pakke (pkg) og navnet på den kjørbare filen (binName)
+  patchWayland = pkg: binName: pkg.overrideAttrs (oldAttrs: {
+    # Vi må ha 'makeWrapper' verktøyet tilgjengelig under byggingen
+    nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [ pkgs.makeWrapper ];
+    
+    # Etter at pakken er installert, pakker vi den inn med nye flagg
+    postFixup = (oldAttrs.postFixup or "") + ''
+      wrapProgram $out/bin/${binName} \
+        --add-flags "${waylandJavaFlags}"
+    '';
+  });
+in
 {
-  # Apps
   environment.systemPackages = with pkgs; [
-    android-studio-full
-    jetbrains.idea
+    # 3. Bruk funksjonen på pakkene
+    
+    # For Android Studio (binæren heter vanligvis 'android-studio')
+    (patchWayland android-studio-full "android-studio")
+
+    # For IntelliJ. NB: Sjekk om du bruker idea-community eller idea-ultimate
+    # Binæren heter vanligvis 'idea-community' eller 'idea-ultimate'
+    (patchWayland jetbrains.idea-community "idea-community")
   ];
 }
