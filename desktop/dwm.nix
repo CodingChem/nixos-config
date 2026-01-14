2141e51b06d2fe72f03b42ac62d73c449375a230
Vegard Pareli Seines <vegsei@gmail.com>
2026-01-14 08:47:53 +0100

    trying dwm

---
+++ desktop/dwm.nix
@@ -0,0 +1,18 @@
+{pkgs, ... }:
+
+{
+  services.xserver = {
+    enable = true;
+    autoRepeatDelay = 200;
+    autoRepeatInterval = 35;
+    windowManager.dwm = {
+      enable = true;
+      package = pkgs.dwm.overrideAttrs {
+        src = ./config/nixos/desktop/dwm;
+      };
+    };
+  };
+  environment.systemPackages = with pkgs; [
+    kitty
+  ];
+};
