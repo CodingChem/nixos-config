ae8d09ea2d1037d680c12d53b022f886a44e400e
Vegard Pareli Seines <vegsei@gmail.com>
2026-01-14 13:03:55 +0100

    add gemini hotkey

--- src/dwm/config.def.h
+++ src/dwm/config.def.h
@@ -62,12 +62,12 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_base, "-nf", col_text, "-sb", col_accent, "-sf", col_active_text, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
-static const char *gemini     = { "chromium" , "--app=https://gemini.google.com", NULL };
+static const char *gemini[]   = { "chromium" , "--app=https://gemini.google.com", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_a,      spawm,          {.v = gemini  }  },
+	{ MODKEY,                       XK_a,      spawn,          {.v = gemini  }  },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
