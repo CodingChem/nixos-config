90c46315993d56832d7de0ffeff231fd70b23f78
Vegard Pareli Seines <vegsei@gmail.com>
2026-01-14 13:43:27 +0100

    wallpaper

--- desktop/dwm.nix
+++ desktop/dwm.nix
@@ -13,20 +13,7 @@
     };
     displayManager = {
       sessionCommands = ''
-      # 1. set backgroundcolor
-      ${pkgs.xorg.xsetroot}/bin/xsetroot -solid "#24273a"
-      # 2. wallpaper loop
-      sh -c '
-        while true; do
-	  # random file
-	  if [ -d "$HOME/Pictures/wallpapers" ]; then
-             ${pkgs.findutils}/bin/find "$HOME/Pictures/wallpapers" -type f \
-             | ${pkgs.coreutils}/bin/shuf -n 1 \
-             | ${pkgs.findutils}/bin/xargs ${pkgs.xwallpaper}/bin/xwallpaper --zoom
-          fi
-          sleep 1m
-	done
-      ' &
+      xwallpaper --zoom ~/Pictures/wallpapers/wall1.jpg
       '';
     };
   };
