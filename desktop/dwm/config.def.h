4b5c4a1ade21d16159d648cf44fd14d053cbf73d
Vegard Pareli Seines <vegsei@gmail.com>
2026-01-14 12:26:30 +0100

    config dwm

--- desktop/dwm/config.def.h
+++ desktop/dwm/config.def.h
@@ -6,16 +6,18 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=16" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=16;
+// Catppuccin Macchiato Palette
+static const char col_base[]        = "#24273a"; // Background
+static const char col_text[]        = "#cad3f5"; // Foreground
+static const char col_surface0[]    = "#363a4f"; // Unfocused border
+static const char col_accent[]      = "#8aadf4"; // Active border/bar (Blue)
+static const char col_active_text[] = "#181926"; // Text on active bar (Crust)
+
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/* fg               bg          border   */
+	[SchemeNorm] = { col_text,        col_base,   col_surface0 },
+	[SchemeSel]  = { col_active_text, col_accent, col_accent   },
 };
 
 /* tagging */
@@ -46,7 +48,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -58,7 +60,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_base, "-nf", col_text, "-sb", col_accent, "-sf", col_active_text, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 static const Key keys[] = {
