b6a87324a9e6228001fec4eb8e01da647f67526c
Vegard Pareli Seines <vegsei@gmail.com>
2026-01-14 12:46:39 +0100

    wallpaper

--- desktop/dwm.nix
+++ desktop/dwm.nix
@@ -11,10 +11,30 @@
         src = ./../src/dwm;
       };
     };
+    displayManager = {
+      ly.enable = true;
+      sessionCommands = ''
+      # 1. set backgroundcolor
+      ${pkgs.xorg.xsetroot}/bin/xsetroot -solid "#24273a"
+      # 2. wallpaper loop
+      sh -c '
+        while true; do
+	  # random file
+	  if [ -d "$HOME/Pictures/wallpapers" ]; then
+             ${pkgs.findutils}/bin/find "$HOME/Pictures/wallpapers" -type f \
+             | ${pkgs.coreutils}/bin/shuf -n 1 \
+             | ${pkgs.findutils}/bin/xargs ${pkgs.xwallpaper}/bin/xwallpaper --zoom
+          fi
+          sleep 1m
+	done
+      ' &
+      '';
+    };
   };
-  services.displayManager.ly.enable = true;
+  
   environment.systemPackages = with pkgs; [
     alacritty
     xclip
+    xwallpaper
   ];
 }
