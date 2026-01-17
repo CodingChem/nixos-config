f39135cc1c2a090a5b7265b6d684f01b317a6e73
Vegard Pareli Seines <vegsei@gmail.com>
2026-01-17 05:53:59 +0100

    mime types

--- desktop/dwm/dwm.nix
+++ desktop/dwm/dwm.nix
@@ -98,6 +98,18 @@ in
         brightnessctl
       ];
 
+      xdg.mimeApps = {
+        enable = true;
+        defaultApplications = {
+          "text/html" = [ "app.zen_browser.zen" ];
+          "x-scheme-handler/http" = [ "app.zen_browser.zen" ];
+          "x-scheme-handler/https" = [ "app.zen_browser.zen" ];
+          "x-scheme-handler/about" = [ "app.zen_browser.zen" ];
+          "x-scheme-handler/unknown" = [ "app.zen_browser.zen" ];
+          # Add more MIME types as needed
+        };
+      };
+
       services.picom = {
         enable = true;
 
