55bf44a7e37fb65d456431aaf6197129e76c3703
Vegard Pareli Seines <vegsei@gmail.com>
2026-05-05 10:54:11 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "My Nixos configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+  outputs = { self, nixpkgs, ... }: {
+      nixosConfigurations.E15 = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+	  modules = [
+	      ./configuration.nix
+	  ];
+      };
+  };
+}
