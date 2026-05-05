f84e4d53d94e9fac0cd84f5e5f62c641cdddab54
Vegard Pareli Seines <vegsei@gmail.com>
2026-05-05 10:58:01 +0200

    small fix

--- configuration.nix
+++ configuration.nix
@@ -80,7 +80,7 @@
   nix.settings = {
       experimental-features = [ "nix-command" "flakes" ];
       auto-optimise-store = true;
-  }
+  };
   nix.gc = {
       automatic = true;
       dates = "weekly";
