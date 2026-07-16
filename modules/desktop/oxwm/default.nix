c56dac8dfe5d3ce3fc5f2455ad3006ebbdbd7922
= <vegsei@gmail.com>
2026-07-16 14:47:02 +0200

    fixing oxwm

--- modules/desktop/oxwm/default.nix
+++ modules/desktop/oxwm/default.nix
@@ -4,6 +4,7 @@
   ...
 }:
 {
+  programs.fuse.enable = true;
   environment.systemPackages = with pkgs; [
     dmenu
     kitty
@@ -12,6 +13,7 @@
   services.displayManager.ly.enable = true;
   services.xserver.windowManager.oxwm.enable = true;
   services.xserver.enable = true;
+  services.xserver.updateDbusEnvironment = true;
   services.dbus.enable = true;
   xdg.portal  = {
     enable = true;
