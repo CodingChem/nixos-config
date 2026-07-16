adac32965481f9df2c5ce26471e3dfdaaed3f225
= <vegsei@gmail.com>
2026-07-16 14:41:17 +0200

    fixing oxwm

--- modules/desktop/oxwm/default.nix
+++ modules/desktop/oxwm/default.nix
@@ -12,4 +12,15 @@
   services.displayManager.ly.enable = true;
   services.xserver.windowManager.oxwm.enable = true;
   services.xserver.enable = true;
+  services.dbus.enable = true;
+  xdg.portal  = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    config.common.default = "*";
+  };
+  services.picom = {
+    enable = true;
+    fade = true;
+    shadow = true;
+  };
 }
