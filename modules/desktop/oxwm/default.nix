646fdb49c8465a635cfc851d87418ed38083ad6f
= <vegsei@gmail.com>
2026-07-16 13:48:12 +0200

    configure minimum oxwm

--- modules/desktop/oxwm/default.nix
+++ modules/desktop/oxwm/default.nix
@@ -4,5 +4,10 @@
   ...
 }:
 {
+  users.users.vegard.packages = with pkgs; [
+    dmenu
+    xclip
+  ];
+  services.displayManager.ly.enable = true;
   services.xserver.windowManager.oxwm.enable = true;
 }
