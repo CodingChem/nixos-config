8ef3ce3612d46655a9261b3c190c191b95877e72
= <vegsei@gmail.com>
2026-07-16 15:03:35 +0200

    fixing oxwm

--- hosts/legioni5/configuration.nix
+++ hosts/legioni5/configuration.nix
@@ -54,10 +54,11 @@
     package = config.boot.kernelPackages.nvidiaPackages.stable;
 
     prime = {
-      offload = {
-        enable = true;
-        enableOffloadCmd = true;
-      };
+      # offload = {
+      #   enable = true;
+      #   enableOffloadCmd = true;
+      # };
+      sync.enable = true;
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:2:0:0";
     };
