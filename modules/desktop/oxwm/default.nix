59eae363556ca5115740b0f628236e2b289e2fd6
Vegard Pareli Seines <vegsei@gmail.com>
2026-08-10 14:08:22 +0200

    add oxwm config to repo

--- modules/desktop/oxwm/default.nix
+++ modules/desktop/oxwm/default.nix
@@ -31,6 +31,7 @@ in
       extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
       config.common.default = "*";
     };
+    xdg.configFile."oxwm/config.lua".source = ./config.lua
     services.picom = {
       enable = true;
       fade = true;
