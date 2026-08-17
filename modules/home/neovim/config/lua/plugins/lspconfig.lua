7b4010cb5868aef964284c0f731c35c12dba93d1
= <vegsei@gmail.com>
2026-08-17 08:10:11 +0200

    add lspconfig and tree-sitter + lsps

--- modules/home/neovim/config/lua/plugins/lspconfig.lua
+++ modules/home/neovim/config/lua/plugins/lspconfig.lua
@@ -1,56 +1,59 @@
 return {
   "neovim/nvim-lspconfig",
   dependencies = {
-    "saghen/blink.cmp", -- or whatever completion engine you use
+    "saghen/blink.cmp",
+    "nvim-telescope/telescope.nvim",
   },
   config = function()
-    local lspconfig = require("lspconfig")
     local blink = require("blink.cmp")
 
-    -- 1. Hook blink.cmp capabilities
-    -- If you have custom capabilities to pass, you can pass them as an argument: blink.get_lsp_capabilities(my_custom_caps)
-    local capabilities = blink.get_lsp_capabilities()
+    -- 1. Hook blink.cmp capabilities globally for all LSPs
+    vim.lsp.config("*", {
+      capabilities = blink.get_lsp_capabilities(),
+    })
 
-    -- 2. Common on_attach function
-    local on_attach = function(client, bufnr)
-      local map = function(keys, func, desc)
-        vim.keymap.set("n", keys, func, { buffer = bufnr, desc = "LSP: " .. desc })
-      end
+    -- 2. Modern on_attach equivalent via LspAttach autocommand
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("UserLspConfig", { clear = true }),
+      callback = function(event)
+        local bufnr = event.buf
+        local client = vim.lsp.get_client_by_id(event.data.client_id)
+        local tb = require("telescope.builtin")
 
-      local tb = require("telescope.builtin")
+        local map = function(keys, func, desc)
+          vim.keymap.set("n", keys, func, { buffer = bufnr, desc = "LSP: " .. desc })
+        end
 
-      -- ==========================================
-      -- Code Navigation (Telescope)
-      -- ==========================================
-      map("gd", tb.lsp_definitions, "Goto Definition (Telescope)")
-      map("gD", vim.lsp.buf.declaration, "Goto Declaration")
-      map("gi", tb.lsp_implementations, "Goto Implementation (Telescope)")
-      map("gt", tb.lsp_type_definitions, "Type Definition (Telescope)")
-      map("gr", tb.lsp_references, "References (Telescope)")
-      map("<leader>ss", tb.lsp_document_symbols, "Document Symbols (Telescope)")
-      map("<leader>sS", tb.lsp_workspace_symbols, "Workspace Symbols (Telescope)")
+        -- ==========================================
+        -- Code Navigation (Telescope)
+        -- ==========================================
+        map("gd", tb.lsp_definitions, "Goto Definition (Telescope)")
+        map("gD", vim.lsp.buf.declaration, "Goto Declaration")
+        map("gi", tb.lsp_implementations, "Goto Implementation (Telescope)")
+        map("gt", tb.lsp_type_definitions, "Type Definition (Telescope)")
+        map("gr", tb.lsp_references, "References (Telescope)")
+        map("<leader>ss", tb.lsp_document_symbols, "Document Symbols (Telescope)")
+        map("<leader>sS", tb.lsp_workspace_symbols, "Workspace Symbols (Telescope)")
 
-      -- ==========================================
-      -- Code Actions & Standard LSP Helpers
-      -- ==========================================
-      map("K", vim.lsp.buf.hover, "Hover Documentation")
-      map("<leader>rn", vim.lsp.buf.rename, "Rename Symbol")
-      map("<leader>ca", vim.lsp.buf.code_action, "Code Action")
+        -- ==========================================
+        -- Code Actions & Standard LSP Helpers
+        -- ==========================================
+        map("K", vim.lsp.buf.hover, "Hover Documentation")
+        map("<leader>rn", vim.lsp.buf.rename, "Rename Symbol")
+        map("<leader>ca", vim.lsp.buf.code_action, "Code Action")
 
-      -- Inlay Hints Toggle (Neovim 0.10+)
-      if client.supports_method("textDocument/inlayHint") then
-        map("<leader>th", function()
-          vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr }), { bufnr = bufnr })
-        end, "Toggle Inlay Hints")
-      end
-    end
+        -- Inlay Hints Toggle
+        if client and client.supports_method("textDocument/inlayHint") then
+          map("<leader>th", function()
+            local is_enabled = vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr })
+            vim.lsp.inlay_hint.enable(not is_enabled, { bufnr = bufnr })
+          end, "Toggle Inlay Hints")
+        end
+      end,
+    })
 
-    --------------------------------------------------
-    -- Lua LSP (lua_ls)
-    --------------------------------------------------
-    lspconfig.lua_ls.setup({
-      on_attach = on_attach,
-      capabilities = capabilities,
+    -- 3. Define Server Configurations
+    vim.lsp.config["lua_ls"] = {
       settings = {
         Lua = {
           runtime = {
@@ -70,14 +73,9 @@ return {
           },
         },
       },
-    })
+    }
 
-    --------------------------------------------------
-    -- Nix LSP (nil_ls)
-    --------------------------------------------------
-    lspconfig.nil_ls.setup({
-      on_attach = on_attach,
-      capabilities = capabilities,
+    vim.lsp.config["nil_ls"] = {
       settings = {
         ["nil"] = {
           formatting = {
@@ -85,14 +83,9 @@ return {
           },
         },
       },
-    })
+    }
 
-    --------------------------------------------------
-    -- JavaScript / TypeScript LSP (vtsls)
-    --------------------------------------------------
-    lspconfig.vtsls.setup({
-      on_attach = on_attach,
-      capabilities = capabilities,
+    vim.lsp.config["vtsls"] = {
       settings = {
         typescript = {
           inlayHints = {
@@ -110,6 +103,9 @@ return {
           },
         },
       },
-    })
+    }
+
+    -- 4. Enable configured servers
+    vim.lsp.enable({ "lua_ls", "nil_ls", "vtsls" })
   end,
 }
