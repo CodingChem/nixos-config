f46a84fb3bd66168230b9d5748ff3be3f7a4cf96
= <vegsei@gmail.com>
2026-08-17 08:11:42 +0200

    add lspconfig and tree-sitter + lsps

--- modules/home/neovim/config/lua/plugins/tree-sitter.lua
+++ modules/home/neovim/config/lua/plugins/tree-sitter.lua
@@ -1,37 +1,28 @@
-require("nvim-treesitter.configs").setup({
-  -- Parsers to ensure installed for Lua, Nix, and JS/TS
-  ensure_installed = {
-    "lua",
-    "luadoc",
-    "nix",
-    "javascript",
-    "typescript",
-    "tsx",
-    "jsdoc",
-    "json",
-    "html",
-    "css",
-  },
-
-  -- Set to false on NixOS if you manage grammar packages via Nixpkgs
-  auto_install = true,
-
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-
-  indent = {
-    enable = true,
-  },
-
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "<C-space>",
-      node_incremental = "<C-space>",
-      scope_incremental = false,
-      node_decremental = "<bs>",
-    },
-  },
-})
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "lua",
+        "luadoc",
+        "nix",
+        "javascript",
+        "typescript",
+        "tsx",
+        "jsdoc",
+        "json",
+        "html",
+        "css",
+      },
+      auto_install = true,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      indent = {
+        enable = true,
+      },
+    })
+  end,
+}
