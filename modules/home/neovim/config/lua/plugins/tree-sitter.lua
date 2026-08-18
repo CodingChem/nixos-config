90357e3e6be62674b103fd3bf9db57dd8e2dd24b
Vegard Pareli Seines <vegsei@gmail.com>
2026-08-18 12:45:41 +0200

    add markdown treesitter grammar

--- modules/home/neovim/config/lua/plugins/tree-sitter.lua
+++ modules/home/neovim/config/lua/plugins/tree-sitter.lua
@@ -1,36 +1,36 @@
+-- Disable lazy.nvim management so it never clones from GitHub
+-- Nix already placed nvim-treesitter on Neovim's runtimepath
 return {
-  "nvim-treesitter/nvim-treesitter",
-  lazy = false,
-  priority = 1000,
-  -- Dynamically resolve the Nix store path from runtimepath
-  -- Prevents lazy.nvim from cloning from GitHub and shadowing Nix query files
-  dir = (function()
-    for _, path in ipairs(vim.opt.rtp:get()) do
-      if path:match("nvim%-treesitter") and not path:match("lazy") then
-        return path
+  {
+    "nvim-treesitter/nvim-treesitter",
+    enabled = false, -- Stops lazy from cloning or touching it
+  },
+  {
+    -- Run our config against the Nix-installed treesitter
+    "nvim-treesitter-config",
+    virtual = true,
+    lazy = false,
+    priority = 1000,
+    config = function()
+      local status_ok, ts_configs = pcall(require, "nvim-treesitter.configs")
+      if not status_ok then
+        return
       end
-    end
-    return nil
-  end)(),
-  config = function()
-    local status_ok, ts_configs = pcall(require, "nvim-treesitter.configs")
-    if not status_ok then
-      return
-    end
 
-    ts_configs.setup({
-      ensure_installed = {},
-      sync_install = false,
-      auto_install = false,
-      ignore_install = {},
+      ts_configs.setup({
+        ensure_installed = {},
+        sync_install = false,
+        auto_install = false,
+        ignore_install = {},
 
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = {
-        enable = true,
-      },
-    })
-  end,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        indent = {
+          enable = true,
+        },
+      })
+    end,
+  },
 }
