d48178c217cd4811d8896e6570ebf216f4be8ada
Vegard Pareli Seines <vegsei@gmail.com>
2026-08-21 15:37:24 +0200

    add noctalia

--- modules/desktop/noctalia/default.nix
+++ modules/desktop/noctalia/default.nix
@@ -12,6 +12,21 @@ in
     services.power-profiles-daemon.enable = true;
     services.upower.enable = true;
 
+    users.users.vegard.packages = with pkgs; [
+      rofi
+        swaybg
+    ];
+    services.displayManager.ly.enable = true;
+    programs.hyprland = {
+      enable = true;
+      xwayland.enable = true;
+      package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+      portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+    };
+    environment.systemPackages = with pkgs; [
+      hyprpolkitagent
+    ];
+
     home-manager.users.vegard = {
       imports = [
         inputs.noctalia.homeModules.default
