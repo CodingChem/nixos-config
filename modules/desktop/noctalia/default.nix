61ab4a150dea52c5da1389d38e290b85772e0fb4
= <vegsei@gmail.com>
2026-09-04 22:43:09 +0200

    remove noctalia

--- modules/desktop/noctalia/default.nix
+++ modules/desktop/noctalia/default.nix
@@ -31,23 +31,6 @@ in
       imports = [
         inputs.noctalia.homeModules.default
       ];
-
-      programs.noctalia = {
-        enable = true;
-
-        settings = { # This may also be a string or path to a .toml file.
-          theme = {
-            mode = "dark";
-            source = "builtin";
-            builtin = "Catppuccin";
-          };
-
-          wallpaper = {
-            enabled = true;
-            default.path = "~/Pictures/walls/wall.jpg";
-          };
-        };
-      };
     };
   };
 }
