cf4e1c11708b045c0fce486c7968ecd3a7bec4f0
= <vegsei@gmail.com>
2026-09-05 22:36:04 +0200

    add xdg desktop

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -26,4 +26,12 @@ in
       quickshell
     ];
   };
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-hyprland
+      xdg-desktop-portal-gtk
+      ];
+      config.common.default = [ "hyprland" "gtk" ];
+  };
 }
