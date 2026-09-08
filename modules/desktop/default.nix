100fb645fb40a67ff162bac5774cd92c7ced03cb
= <vegsei@gmail.com>
2026-09-08 11:13:32 +0200

    fix networking

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -54,13 +54,10 @@ in
       services.pulseaudio.enable = false;
       security.rtkit.enable = true;
       services.pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-      services.resolved.enable = true;
-      networking.networkmanager.dns = "systemd-resolved";
-      services.resolved.fallbackDns = [ "1.1.1.1" "8.8.8.8" ];
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
 # If you want to use JACK applications, uncomment this
 #jack.enable = true;
 
@@ -68,6 +65,9 @@ in
 # no need to redefine it in your config for now)
 #media-session.enable = true;
       };
+      services.resolved.enable = true;
+      networking.networkmanager.dns = "systemd-resolved";
+      services.resolved.fallbackDns = [ "1.1.1.1" "8.8.8.8" ];
       environment.systemPackages = [
         myDesktopScripts
         pkgs.playerctl
